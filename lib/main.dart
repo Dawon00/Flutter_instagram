766afd243781f5700fb77035fd3abf37352fe7a0
Dawon00 <da6921@naver.com>
2022-07-06 21:55:50 +0900

    Feat: posting view

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 import './style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(theme: style.theme, home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0; //현재 탭
+  var data = [];
+
+  getData() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+
+    var result2 = jsonDecode(result.body);
+    setState(() {
+      data = result2;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +48,17 @@ class MyApp extends StatelessWidget {
           )
         ],
       ),
-      body: Text('test message'),
+      body: [Home(data: data), Text('shop page')][tab],
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (i) {
+          setState(() {
+            tab = i;
+          });
+        },
         iconSize: 25,
-        items: <BottomNavigationBarItem>[
+        items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.home_outlined), label: 'home'),
           BottomNavigationBarItem(
@@ -33,5 +66,31 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key, this.data}) : super(key: key);
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    if (data.isNotEmpty) {
+      return ListView.builder(
+          itemCount: 3,
+          itemBuilder: (c, i) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(data[i]['image']),
+                Text(data[i]['user']),
+                Text(data[i]['likes'].toString()),
+                Text(data[i]['content']),
+              ],
+            );
+          });
+    } else {
+      return Text('loading...');
+    }
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 var theme = ThemeData(
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black,
+    ),
     appBarTheme: AppBarTheme(
       titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
       color: Colors.white,
       elevation: 1,
       actionsIconTheme: IconThemeData(color: Colors.black),
     ),
-    textTheme: TextTheme(bodyText2: TextStyle(color: Colors.red)));
+    textTheme: TextTheme(bodyText2: TextStyle(color: Colors.black)));
